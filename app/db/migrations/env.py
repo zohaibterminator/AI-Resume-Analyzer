692b64ab4485616a11fc9465b0b7bc24c4ccae2f
zohaibterminator <zohaibsaqib803@gmail.com>
2025-04-17 23:52:35 +0500

    performed first migration

--- app/db/migrations/env.py
+++ app/db/migrations/env.py
@@ -6,10 +6,16 @@ from sqlalchemy import pool
 from alembic import context
 from app.db.models import Base
 
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", os.getenv("DATABASE_URL"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
---
+++ app/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from db.database import engine, Base
+from routers import resume, recommendations
+import uvicorn
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(
+    title="AI Resume Analyzer",
+    version="1.0.0",
+)
+
+# CORS config
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(resume.router, prefix="/resume", tags=["Resume"])
+app.include_router(recommendations.router, prefix="/recommend", tags=["Recommendations"])
+
+
+@app.get("/")
+async def root():
+    return {"message": "Al Okay"}
+
+
+# Run with uvicorn
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
